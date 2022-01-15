5db93422946ad00bdaeaec76d39db34c142ab0ce
Josh Higgins <5124298+joshiggins@users.noreply.github.com>
2022-01-15 21:12:46 +0000

    add basic tests

--- tests/example/urls.py
+++ tests/example/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "example"
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
+    path("basic", views.BasicFunctionalityView.as_view(), name="basic"),
 ]
--- tests/example/views.py
+++ tests/example/views.py
@@ -7,3 +7,16 @@ class Index(TemplateView):
 
 class Navigation(TemplateView):
     template_name = "example/nav.html"
+
+
+class TestView(TemplateView):
+    template_name = "example/test.html"
+
+
+class BasicFunctionalityView(TemplateView):
+    template_name = "example/basic.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["test_view_str"] = 'example.views.TestView'
+        return context
