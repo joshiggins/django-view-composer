446f577491ee133e5fba156fe057d00629976c6b
Josh Higgins <5124298+joshiggins@users.noreply.github.com>
2022-01-15 10:27:37 +0000

    add a view tag to the example

--- tests/example/urls.py
+++ tests/example/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "test_app"
+app_name = "example"
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
 ]
--- tests/example/views.py
+++ tests/example/views.py
@@ -3,3 +3,7 @@ from django.views.generic.base import TemplateView
 
 class Index(TemplateView):
     template_name = "example/index.html"
+
+
+class Navigation(TemplateView):
+    template_name = "example/nav.html"
