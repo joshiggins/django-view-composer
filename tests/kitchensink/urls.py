2135d0ff8a38b17a48c3b38576922a7f59740429
Josh Higgins <5124298+joshiggins@users.noreply.github.com>
2022-01-16 17:52:35 +0000

    add createview + listview example

--- tests/kitchensink/urls.py
+++ tests/kitchensink/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = "kitchensink"
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
+    path("person-list", views.PersonListView.as_view(), name="person-list"),
+    path("person-create", views.PersonCreateView.as_view(), name="person-create"),
 ]
--- tests/kitchensink/views.py
+++ tests/kitchensink/views.py
@@ -1,5 +1,20 @@
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .models import Person
 
 
 class Index(TemplateView):
     template_name = "kitchensink/index.html"
+
+
+class PersonListView(ListView):
+    model = Person
+
+
+class PersonCreateView(CreateView):
+    model = Person
+    fields = ["first_name", "last_name"]
+    success_url = reverse_lazy("kitchensink:person-list")
