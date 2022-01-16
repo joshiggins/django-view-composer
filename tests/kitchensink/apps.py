698eae0a327e0418df4a834a92fe846140849345
Josh Higgins <5124298+joshiggins@users.noreply.github.com>
2022-01-16 15:23:24 +0000

    replace example with the kitchen sink app

--- tests/kitchensink/apps.py
+++ tests/kitchensink/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ExampleConfig(AppConfig):
+class KitchensinkConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'example'
+    name = 'kitchensink'
