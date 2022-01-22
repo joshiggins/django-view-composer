16b2cd29b541deccbfa9102f571504941635849e
Josh Higgins <5124298+joshiggins@users.noreply.github.com>
2022-01-22 14:45:00 +0000

    start adding unit tests

---
+++ tests/basic/tests.py
@@ -0,0 +1,74 @@
+from django.test import TestCase
+from django.template import Context, Template
+from django.test.client import RequestFactory
+import jsonpickle
+
+
+class ViewTagTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def get_with_context(self, template, context):
+        request = self.factory.get("/")
+        t = Template(template)
+        c = Context({"request": request, **context})
+        return t.render(c)
+
+    def test_single_view(self):
+        res = self.get_with_context(
+            "{% load view_composer %}{% view 'basic.views.IndexView' %}", {}
+        )
+        self.assertEqual(res, "<h1>Test</h1>")
+
+    def test_all_context(self):
+        res = self.get_with_context(
+            "{% load view_composer %}{% view 'basic.views.ContextTestView' %}",
+            {"food": "spam"},
+        )
+        test_ctx = jsonpickle.decode(res)
+        self.assertEqual(test_ctx["food"], "spam")
+
+    def test_no_context(self):
+        res = self.get_with_context(
+            "{% load view_composer %}{% view 'basic.views.ContextTestView' with only %}",
+            {"food": "spam"},
+        )
+        test_ctx = jsonpickle.decode(res)
+        self.assertFalse("food" in test_ctx)
+
+    def test_extra_context(self):
+        res = self.get_with_context(
+            "{% load view_composer %}"
+            "{% view 'basic.views.ContextTestView' with food='spam' %}",
+            {},
+        )
+        test_ctx = jsonpickle.decode(res)
+        self.assertEqual(test_ctx["food"], "spam")
+
+    def test_extra_context_override(self):
+        res = self.get_with_context(
+            "{% load view_composer %}"
+            "{% view 'basic.views.ContextTestView' with food='eggs' %}",
+            {"food": "spam"},
+        )
+        test_ctx = jsonpickle.decode(res)
+        self.assertEqual(test_ctx["food"], "eggs")
+
+    def test_extra_context_only(self):
+        res = self.get_with_context(
+            "{% load view_composer %}"
+            "{% view 'basic.views.ContextTestView' with ham=1 only %}",
+            {"food": "spam"},
+        )
+        test_ctx = jsonpickle.decode(res)
+        self.assertEqual(test_ctx["ham"], 1)
+        self.assertFalse("food" in test_ctx)
+
+    def test_extra_context_resolved(self):
+        res = self.get_with_context(
+            "{% load view_composer %}"
+            "{% view 'basic.views.ContextTestView' with food=spam %}",
+            {"spam": "eggs"},
+        )
+        test_ctx = jsonpickle.decode(res)
+        self.assertEqual(test_ctx["food"], "eggs")
---
+++ tests/basic/views.py
@@ -0,0 +1,14 @@
+from django.views.generic.base import TemplateView
+import jsonpickle
+
+
+class IndexView(TemplateView):
+    template_name = "basic/index.html"
+
+
+class ContextTestView(TemplateView):
+    template_name = "basic/context.html"
+
+    def get_context_json(self):
+        context = self.get_context_data()
+        return jsonpickle.encode(context)
