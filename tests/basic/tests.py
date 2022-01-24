aec96642d38a534b2f3a058c8fd95babdfc350b0
Josh Higgins <5124298+joshiggins@users.noreply.github.com>
2022-01-24 22:26:51 +0000

    allow passing view kwargs before 'with'

--- tests/basic/tests.py
+++ tests/basic/tests.py
@@ -98,3 +98,12 @@ class ViewTagTestCase(TestCase):
         )
         test_ctx = jsonpickle.decode(res)
         self.assertEqual(test_ctx["food"], "spam")
+
+    def test_view_kwargs(self):
+        res = self.get_with_context(
+            "{% load view_composer %}"
+            "{% view 'basic.views.KwargsTestView' food='spam' with ham='eggs' %}", {}
+        )
+        test_ctx = jsonpickle.decode(res)
+        self.assertEqual(test_ctx["food_kwarg"], "spam")
+        self.assertEqual(test_ctx["ham"], "eggs")
--- tests/basic/views.py
+++ tests/basic/views.py
@@ -10,9 +10,16 @@ class ContextTestView(TemplateView):
     template_name = "basic/context.html"
 
     def get_context_json(self):
-        context = self.get_context_data()
+        context = self.get_context_data(**self.kwargs)
         return jsonpickle.encode(context)
 
 
 class BlockTestView(TemplateView):
     template_name = "basic/block.html"
+
+
+class KwargsTestView(ContextTestView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["food_kwarg"] = kwargs["food"]
+        return context
